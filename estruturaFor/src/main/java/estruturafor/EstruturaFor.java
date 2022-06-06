5aa08e85ea2ea1995fb9ea2ed1245763b133536f
Caio Kenedy <caiokenedysantos@gmail.com>
2022-06-06 13:55:55 -0300

    for

---
+++ estruturaFor/src/main/java/estruturafor/EstruturaFor.java
@@ -0,0 +1,13 @@
+
+package estruturafor;
+
+
+public class EstruturaFor {
+
+    public static void main(String[] args) {
+       
+        for(int i = 0; i <= 10; i++) {
+            System.out.print(i);
+        }
+    }
+}
