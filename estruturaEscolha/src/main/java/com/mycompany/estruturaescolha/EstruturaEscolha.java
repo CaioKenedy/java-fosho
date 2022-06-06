2a522524f0dd6303df707d879b129eace297b541
Caio Kenedy <caiokenedysantos@gmail.com>
2022-06-06 12:31:09 -0300

    switch

---
+++ estruturaEscolha/src/main/java/com/mycompany/estruturaescolha/EstruturaEscolha.java
@@ -0,0 +1,25 @@
+
+
+package com.mycompany.estruturaescolha;
+
+
+public class EstruturaEscolha {
+
+    public static void main(String[] args) {
+        
+        int codigoProduto = 2;
+        
+        switch(codigoProduto) {
+        
+            case 1:
+                
+                break;
+            
+            case 2:
+                
+                break;
+            
+            default:        
+        }
+    }
+}
