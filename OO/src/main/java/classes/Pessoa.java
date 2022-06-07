3d6fdfd7bcd7b8f2ab836d0c509db291d1092b30
Caio Kenedy <caiokenedysantos@gmail.com>
2022-06-07 18:56:57 -0300

    exemplo metodo construtor

--- OO/src/main/java/classes/Pessoa.java
+++ OO/src/main/java/classes/Pessoa.java
@@ -1,29 +1,32 @@
 package classes;
 
-
 public class Pessoa {
-    
+
     private float peso;
     private float altura;
-    
+
+    public Pessoa() {
+        System.out.println("teste");
+    }
+
     public float calcularIMC() {
         float imc = peso / (altura * altura);
         return imc;
     }
-    
+
     public void setPeso(float peso) {
-        
+
         this.peso = peso;
     }
-    
+
     public float getPeso() {
         return peso;
     }
-    
+
     public void setAltura(float altura) {
         this.altura = altura;
     }
-    
+
     public float getAltura() {
         return altura;
     }
